1a8517b1e23924076bbf0d0ae3f8bc85c01f1b7c
RachaneeSaengkrajai <rachanee.saeng@gmail.com>
2019-03-04 17:32:19 +0700

    Move query Handler to base class

---
+++ dotnetcoresample/Customers/BaseRedisRequestHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace dotnetcoresample.Customers
+{
+    public abstract class BaseRedisRequestHandler<TRequest, TResponse> 
+        : IRequestHandler<TRequest, TResponse>
+        where TRequest: IRequest<TResponse>
+    {
+        protected readonly DotNetSampleDbContext _context;
+        private readonly ConnectionMultiplexer _redis;
+        protected readonly IDatabase _redisdb;
+
+        public BaseRedisRequestHandler(DotNetSampleDbContext context, ConnectionMultiplexer redis)
+        {
+            _context = context;
+            _redis = redis;
+            _redisdb = redis.GetDatabase();
+        }
+        
+        public abstract Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken);
+    }
+}
--- dotnetcoresample/Customers/Queries/GetTopName/GetCustomerDetailQueryHandler.cs
+++ dotnetcoresample/Customers/Queries/GetTopName/GetCustomerDetailQueryHandler.cs
@@ -9,25 +9,21 @@ using System.Threading.Tasks;
 
 namespace dotnetcoresample.Customers.Queries.GetTopName
 {
-    public class GetTopNameQueryHandler : IRequestHandler<GetTopNameQuery, string>
+    public class GetTopNameQueryHandler : BaseRedisRequestHandler<GetTopNameQuery, string>
     {
-        private readonly ConnectionMultiplexer _redis;
-        private readonly IDatabase _database;
-
-        public GetTopNameQueryHandler(ConnectionMultiplexer redis)
+        public GetTopNameQueryHandler(DotNetSampleDbContext context, ConnectionMultiplexer redis) 
+            : base(context, redis)
         {
-            _redis = redis;
-            _database = redis.GetDatabase();
         }
 
-        public Task<string> Handle(GetTopNameQuery request, CancellationToken cancellationToken)
+        public override Task<string> Handle(GetTopNameQuery request, CancellationToken cancellationToken)
         {
             return GetTopName();
         }
 
         private async Task<string> GetTopName()
         {
-            var data = await _database.StringGetAsync("TopName");
+            var data = await _redisdb.StringGetAsync("TopName");
 
             if (data.IsNullOrEmpty)
             {
