7ffc6d8a59ee6e4defbc0ebb81030968b25c721a
RachaneeSaengkrajai <rachanee.saeng@gmail.com>
2019-03-06 18:05:20 +0700

    Remove nswag

--- dotnetcoresample/Startup.cs
+++ dotnetcoresample/Startup.cs
@@ -102,11 +102,11 @@ namespace dotnetcoresample
 
             app.UseStaticFiles();
 
-            app.UseSwaggerUi3(settings =>
-            {
-                settings.Path = "/api";
-                settings.DocumentPath = "/api/specification.json";
-            });
+            //app.UseSwaggerUi3(settings =>
+            //{
+            //    settings.Path = "/api";
+            //    settings.DocumentPath = "/api/specification.json";
+            //});
 
             app.UseMvc(routes =>
             {
@@ -139,9 +139,9 @@ namespace dotnetcoresample
 
         private void ConfigureEventBus(IApplicationBuilder app)
         {
-            //var eventBus = app.ApplicationServices.GetRequiredService<IEventBus>();
+            var eventBus = app.ApplicationServices.GetRequiredService<IEventBus>();
 
-            //eventBus.Subscribe<CacheValueChangedIntegrationEvent, CacheValueChangedIntegrationEventHandler>();
+            eventBus.Subscribe<CacheValueChangedIntegrationEvent, CacheValueChangedIntegrationEventHandler>();
         }
     }
 }
